729f8ab2738fad1532657d3864ae8bdd709f58a8
Édipo Costa Rebouças <edipo.reboucas@yahoo.com.br>
2015-03-28 02:27:56 +0000

    remove outdate code

--- src/Middleware/Entity/Middleware.php
+++ src/Middleware/Entity/Middleware.php
@@ -4,8 +4,6 @@ namespace Middleware\Entity;
 class Middleware
 {
     const CONFIG         = 'middlewares';
-    const CONFIG_LOCAL   = 'local';
     const CONFIG_GLOBAL  = 'global';
-    const HANDLE_METHOD  = 'handle';
     const PROPERTY       = 'middleware';
 }
