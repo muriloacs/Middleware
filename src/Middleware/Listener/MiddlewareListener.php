824c5b08c63a6e626ff08089b316162295b62d34
Murilo Santiago <murilo.santiago@esmart.com.br>
2015-03-30 18:42:28 -0300

    DocBlocks

--- src/Middleware/Listener/MiddlewareListener.php
+++ src/Middleware/Listener/MiddlewareListener.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Murilo Amaral (http://muriloamaral.com/)
+ * Édipo Rebouças (http://edipo.com.br)
+ *
+ * @link      https://github.com/muriloacs/Middleware
+ * @copyright Copyright (c) 2015 Murilo Amaral
+ * @license   The MIT License (MIT)
+ * @since     File available since Release 1.0
+ */
+
 namespace Middleware\Listener;
 
 use Zend\EventManager\ListenerAggregateInterface;
--- tests/MiddlewareTest/Service/Factory/MiddlewareServiceFactoryTest.php
+++ tests/MiddlewareTest/Service/Factory/MiddlewareServiceFactoryTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Murilo Amaral (http://muriloamaral.com/)
+ * Édipo Rebouças (http://edipo.com.br)
+ *
+ * @link      https://github.com/muriloacs/Middleware
+ * @copyright Copyright (c) 2015 Murilo Amaral
+ * @license   The MIT License (MIT)
+ * @since     File available since Release 1.0
+ */
 
 namespace MiddlewareTest\Service\Factory;
 
