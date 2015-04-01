07118eb1698a5f07e9729b9883d1b8752058b6e2
Édipo Costa Rebouças <edipo.reboucas@yahoo.com.br>
2015-03-31 23:26:50 -0300

    fix code style Module.php, remove php-cs-fixer program

--- Module.php
+++ Module.php
@@ -12,18 +12,6 @@
  * @since File available since Release 1.0
  */
 
-/**
- * Murilo Amaral (http://muriloamaral.com)
- * Édipo Rebouças (http://edipo.com.br).
- *
- * @link      https://github.com/muriloacs/Middleware
- *
- * @copyright Copyright (c) 2015 Murilo Amaral
- * @license   The MIT License (MIT)
- *
- * @since     File available since Release 1.0
- */
-
 namespace Middleware;
 
 use Middleware\Listener\MiddlewareListener;
